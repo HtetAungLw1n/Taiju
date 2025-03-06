56e201d56aed59f297ddbee9bbcbdca0c6443dc6
Htet Aung Lwin <htetaunglwin.canon@gmail.com>
2025-03-06 19:52:48 +0630

    fix bug and smoothen the auth form.jsx

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -7,10 +7,10 @@ const AuthForm = ({ header, button, form, method }) => {
   return (
     <section className="h-screen relative text-deepgreen">
       {/* this is small screen  */}
-      <div className="h-full md:hidden">
+      <div className="h-full lg:hidden">
         <img src={image} alt="image" className="h-full object-cover" />
 
-        <div className="absolute bottom-0 bg-green1 border-t-4 border-deepgreen w-full rounded-t-2xl p-4">
+        <div className="absolute bottom-0 bg-greendark border-t-4 border-deepgreen w-full rounded-t-2xl p-4">
           <div className="w-fit mb-8">
             <Link to={"/welcome"}>
               <FaArrowLeft className="w-4 h-4" />
@@ -20,31 +20,34 @@ const AuthForm = ({ header, button, form, method }) => {
             <div className="text-3xl font-semibold my-4">{header}</div>
 
             <Form method={method} className="flex flex-col py-4">
+              <div className="my-2">
+                <label htmlFor="name" className="text-xl block font-semibold">
+                  UserName
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="border-2 border-deepgreen p-2 rounded-lg bg-white2 w-full outline-none"
+                />
+              </div>
+
               {form === "isSignIn" && (
                 <div className="my-2">
-                  <label htmlFor="name" className="text-xl block font-semibold">
-                    Name
+                  <label
+                    htmlFor="email"
+                    className="text-xl block font-semibold"
+                  >
+                    Email
                   </label>
                   <input
                     type="text"
-                    name="name"
-                    id="name"
+                    name="email"
+                    id="email"
                     className="border-2 border-deepgreen p-2 rounded-lg bg-white2 w-full outline-none"
                   />
                 </div>
               )}
-
-              <div className="my-2">
-                <label htmlFor="email" className="text-xl block font-semibold">
-                  Email
-                </label>
-                <input
-                  type="text"
-                  name="email"
-                  id="email"
-                  className="border-2 border-deepgreen p-2 rounded-lg bg-white2 w-full outline-none"
-                />
-              </div>
               <div className="my-2">
                 <label
                   htmlFor="password"
@@ -80,7 +83,7 @@ const AuthForm = ({ header, button, form, method }) => {
                 </div>
                 <button
                   type="submit"
-                  className="bg-white1 font-semibold mt-4 px-4 py-2 rounded-lg border-2 border-deepgreen block"
+                  className="bg-cream font-semibold mt-4 px-4 py-2 rounded-lg border-2 border-deepgreen block"
                 >
                   {button}
                 </button>
@@ -92,14 +95,14 @@ const AuthForm = ({ header, button, form, method }) => {
       {/* this is small screen  */}
 
       {/* this is large screen */}
-      <div className="hidden md:flex md:h-full">
+      <div className="hidden lg:flex lg:h-full text-night">
         <img
           src={image}
           alt="image"
           className="w-2/3 object-cover object-left"
         />
 
-        <div className="bg-greendark border-deepgreen rounded-l-2xl border-l-4 w-full p-4 px-8">
+        <div className="bg-greendark rounded-l-2xl border-l-4 w-full p-4 px-8">
           <Link to={"/welcome"}>
             <FaArrowLeft className="w-6 h-6" />
           </Link>
@@ -110,7 +113,7 @@ const AuthForm = ({ header, button, form, method }) => {
             <Form method={method} className="flex flex-col py-4">
               <div className="my-2">
                 <label htmlFor="name" className="text-xl block font-semibold">
-                  Name
+                  UserName
                 </label>
                 <input
                   type="text"
@@ -168,7 +171,7 @@ const AuthForm = ({ header, button, form, method }) => {
                 </div>
                 <button
                   type="submit"
-                  className="bg-cream font-semibold mt-4 px-4 py-2 rounded-lg border-2 border-deepgreen block"
+                  className="bg-cream font-semibold mt-4 px-4 py-2 rounded-lg border-2 border-deepgreen block cursor-pointer"
                 >
                   {button}
                 </button>
